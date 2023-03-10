32cffa57129c82f8f0976f43ab525925f658bd64
Tanuki <susann.orosco33@gmail.com>
2023-03-10 12:47:28 -0800

    wrote the beginning of a for loor, wrote down questions for tutoring session

--- Develop/script.js
+++ Develop/script.js
@@ -37,3 +37,22 @@ if (passwordLength <8 || passwordLength > 128) {
 }
 
 console.log(password);
+
+//Questions for Tutor:
+
+// How to have window prompt/alert open while also showing the generator in the background browser?
+// question prcess of generating a password:
+// password length, uppercased characters, lower cased characters, numbers, special characters
+// if everything false, alert: Please select at least one criteria to generate secure password, the more criteria you selcet, the more secure your password will be
+// How to include all arrays in one function word?
+//Questions: Do you want Upper Case Letters in your password?
+//Do you want lower case letters in your password?
+//Do you want numbers in your password?
+//Do you want Symbols in your password?
+
+
+// for loop:
+// else {
+ // for (var i=0, i<passwordLength; i++ ) {
+  //  var random = Math.floor
+ // }
