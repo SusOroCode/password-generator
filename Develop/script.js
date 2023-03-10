e421ac822efe17076a212b73969bb20c4d1dcc56
Tanuki <susann.orosco33@gmail.com>
2023-03-10 12:13:36 -0800

    Created welcome alert and started working on password length question

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,12 @@
 // Assignment code here
 
+// added 4 arrays given by TA Meg Meyers through Slack
+
+var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z',];
+var lowerCasedCharacters = [ 'a','b', 'c','d', 'e','f', 'g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialCharacters = [ '@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.',];
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +22,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Reference: https://developer.mozilla.org/en-US/docs/Web/API/Window/load_event
+// User Starting Point To Generate Password
+
+window.onload = alert('Welcome to the Secure Password Generator! Please click the red button that says "Generate Password" to start!');
+
+
+var passwordLength = prompt("How long do you want your secure password to be?");
+
+if (passwordLength <8 || passwordLength > 128) {
+  alert (" Please choose a password between 8 and 128 characters long.")
+}
+
+console.log(password);
