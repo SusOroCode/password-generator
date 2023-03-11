3c68cd8f91da7386c7ceb5b68f6a76897983179e
Tanuki <susann.orosco33@gmail.com>
2023-03-10 19:26:54 -0800

    worked on numeric and special characters and how to return to the beginning when nothing is selected, added notes

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 
-// added 4 arrays given by TA Meg Meyers through Slack
+// added 4 arrays given by TA through Slack
 
 var upperCaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
 var lowerCaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
@@ -13,28 +13,52 @@ var allChar = [];
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+
+
+// Tutor helped with putting first variable into function and helped with figuring out how to use return
   var passwordLength = prompt("How long do you want your secure password to be?");
 
   if (passwordLength < 8 || passwordLength > 128) {
     alert(" Please choose a password between 8 and 128 characters long.");
     return generatePassword();
-
   }
-  
+// Class Zoom chat, Teacher helped with concat function ( I asked how to put all arrays into one function 
+// so that all characters can be generated )
+// Teacher helped with discerning array variable name from variable function name
   var hasUpperCaseChar = confirm("Do you want upper cased characters in your secure password?");
-  
-   if (hasUpperCaseChar === true) {
+
+  if (hasUpperCaseChar === true) {
     alert("Great! Upper cased characters make your password more secure!");
     allChar = allChar.concat(upperCaseChar);
-   } 
+  }
 
-   var hasLowerCaseChar = confirm("Do you want lower cased characters in your secure password?");
-   if (hasLowerCaseChar === true) {
+  var hasLowerCaseChar = confirm("Do you want lower cased characters in your secure password?");
+
+  if (hasLowerCaseChar === true) {
     alert("Great! Lower cased characters make your password more secure!");
     allChar = allChar.concat(lowerCaseChar);
-   } 
+  }
 
-   console.log(allChar);
+  var hasNumericChar = confirm("Do you want numeric characters in your secure password?");
+  
+  if (hasNumericChar === true) {
+    alert("Great! Numeric characters make your password more secure!");
+    allChar = allChar.concat(numericChar);
+  }
+
+  var hasSpecialChar = confirm("Do you want special characters in your secure password?");
+  
+  if (hasSpecialChar === true) {
+    alert("Great! Special characters make your password more secure!");
+    allChar = allChar.concat(specialChar);
+  } else if (allChar.length === 0) {
+    alert("You didn't pick any characters! We cannot create a password for you without chosing at least one!");
+    return generatePassword();
+
+
+  }
+
+  console.log(allChar);
 }
 
 
