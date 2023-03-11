4bd5e96068573acd7ce2b95e00b340086c6276cb
Tanuki <susann.orosco33@gmail.com>
2023-03-10 19:03:56 -0800

    added upper and lower cased characters in function

--- Develop/script.js
+++ Develop/script.js
@@ -2,11 +2,12 @@
 
 // added 4 arrays given by TA Meg Meyers through Slack
 
-var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
-var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.',];
+var upperCaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',];
+var lowerCaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',];
+var numericChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialChar = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.',];
 
+var allChar = [];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -20,8 +21,20 @@ function generatePassword() {
 
   }
   
+  var hasUpperCaseChar = confirm("Do you want upper cased characters in your secure password?");
+  
+   if (hasUpperCaseChar === true) {
+    alert("Great! Upper cased characters make your password more secure!");
+    allChar = allChar.concat(upperCaseChar);
+   } 
 
+   var hasLowerCaseChar = confirm("Do you want lower cased characters in your secure password?");
+   if (hasLowerCaseChar === true) {
+    alert("Great! Lower cased characters make your password more secure!");
+    allChar = allChar.concat(lowerCaseChar);
+   } 
 
+   console.log(allChar);
 }
 
 
