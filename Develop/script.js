9bbfa56b02bc8d3d09ea4e828c40d69bfb175de5
Tanuki <susann.orosco33@gmail.com>
2023-03-11 19:26:46 -0800

    added for loop, added event listener fpr results

--- Develop/script.js
+++ Develop/script.js
@@ -15,16 +15,18 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
 
 
-// Tutor helped with putting first variable into function and helped with figuring out how to use return
+
+
+  // Tutor helped with putting first variable into function and helped with figuring out how to use return
   var passwordLength = prompt("How long do you want your secure password to be?");
 
   if (passwordLength < 8 || passwordLength > 128) {
     alert(" Please choose a password between 8 and 128 characters long.");
     return generatePassword();
   }
-// Class Zoom chat, Teacher helped with concat function ( I asked how to put all arrays into one function 
-// so that all characters can be generated )
-// Teacher helped with discerning array variable name from variable function name
+  // Class Zoom chat, Teacher helped with concat function ( I asked how to put all arrays into one function 
+  // so that all characters can be generated )
+  // Teacher helped with discerning array variable name from variable function name
   var hasUpperCaseChar = confirm("Do you want upper cased characters in your secure password?");
 
   if (hasUpperCaseChar === true) {
@@ -40,14 +42,14 @@ function generatePassword() {
   }
 
   var hasNumericChar = confirm("Do you want numeric characters in your secure password?");
-  
+
   if (hasNumericChar === true) {
     alert("Great! Numeric characters make your password more secure!");
     allChar = allChar.concat(numericChar);
   }
 
   var hasSpecialChar = confirm("Do you want special characters in your secure password?");
-  
+
   if (hasSpecialChar === true) {
     alert("Great! Special characters make your password more secure!");
     allChar = allChar.concat(specialChar);
@@ -55,12 +57,22 @@ function generatePassword() {
     alert("You didn't pick any characters! We cannot create a password for you without chosing at least one!");
     return generatePassword();
 
+  } 
+  //else {
+   // for (var i = 0; i < passwordLength, i++) {
+   //   var randomPassword = allChar[Math.floor(Math.random() * allChar.length)];
+   //   result.push(randomPassword);
+   // }
+//  }
 
-  }
-
-  console.log(allChar);
 }
 
+//for (var i=0; i< passwordLength, i++) {
+// var randomPassword = allChar[Math.floor(Math.random() * allChar.length)];
+// result.push(randomPassword);
+//}
+
+//console.log(result);
 
 // Write password to the #password input
 function writePassword() {
@@ -74,6 +86,12 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+document.getElementById("password").innerHTML=result.push;
+
+// Resources: https://javascript.info/alert-prompt-confirm
+
+
+
 
 // Reference: https://developer.mozilla.org/en-US/docs/Web/API/Window/load_event
 // User Starting Point To Generate Password
@@ -81,21 +99,6 @@ generateBtn.addEventListener("click", writePassword);
 
 //console.log(generateAPassword);
 
-//Questions for Tutor:
-
-// How to have window prompt/alert open while also showing the generator in the background browser?
-// question prcess of generating a password:
-// password length, uppercased characters, lower cased characters, numbers, special characters
-// if everything false, alert: Please select at least one criteria to generate secure password, the more criteria you selcet, the more secure your password will be
-// How to include all arrays in one function word?
-//Questions: Do you want Upper Case Letters in your password?
-//Do you want lower case letters in your password?
-//Do you want numbers in your password?
-//Do you want Symbols in your password?
 
 
-// for loop:
-// else {
- // for (var i=0, i<passwordLength; i++ ) {
-  //  var random = Math.floor
- // }
+
